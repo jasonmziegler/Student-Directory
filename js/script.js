2f04ec4b87a459cb62212a1475e71ad69b7ea109
Jason Ziegler <jason@insightfulautomation.com>
2021-03-29 20:51:15 -0500

    add createStudentListItemFunction

--- js/script.js
+++ js/script.js
@@ -20,46 +20,49 @@ This function will create and insert/append the elements needed to display a "pa
 
 console.log(data);
 
-// TODO: build a list item with information from the data.js file
+//TODO: Create createStudentListItem function
+function createStudentListItem(studentObject) {
+   //<li class="student-item cf">
+   const studentListItem = document.createElement('li');
+   studentListItem.className = "student-item cf";
+   //<div class="student-details">
+   const studentDetailsDiv = document.createElement('div');
+   studentDetailsDiv.className = "student-details";
+   studentListItem.appendChild(studentDetailsDiv);
+   //  <img class="avatar" src="https://randomuser.me/api/portraits/women/25.jpg" alt="Profile Picture">
+   const avatarImg = document.createElement('img');
+   avatarImg.className = "avatar";
+   avatarImg.src = studentObject.picture.large;
+   avatarImg.alt = "Profile Picture";
+   studentDetailsDiv.appendChild(avatarImg);
+   //  <h3>Ethel Dean</h3>
+   const h3 = document.createElement('h3');
+   h3.textContent = `${studentObject.name.first} ${studentObject.name.last}`;
+   studentDetailsDiv.appendChild(h3);
+   //  <span class="email">ethel.dean@example.com</span>
+   const emailSpan = document.createElement('span');
+   emailSpan.className = "email";
+   emailSpan.innerText = studentObject.email;
+   studentDetailsDiv.appendChild(emailSpan);
+   studentListItem.appendChild(studentDetailsDiv);
+   //</div>
+   //<div class="joined-details">
 
-//<li class="student-item cf">
-const studentListItem = document.createElement('li');
-studentListItem.className = "student-item cf";
-//<div class="student-details">
-const studentDetailsDiv = document.createElement('div');
-studentDetailsDiv.className = "student-details";
-studentListItem.appendChild(studentDetailsDiv);
-//  <img class="avatar" src="https://randomuser.me/api/portraits/women/25.jpg" alt="Profile Picture">
-const avatarImg = document.createElement('img');
-avatarImg.className = "avatar";
-avatarImg.src = "https://randomuser.me/api/portraits/women/25.jpg";
-avatarImg.alt = "Profile Picture";
-studentDetailsDiv.appendChild(avatarImg);
-//  <h3>Ethel Dean</h3>
-const h3 = document.createElement('h3');
-h3.textContent = "Ethel Dean";
-studentDetailsDiv.appendChild(h3);
-//  <span class="email">ethel.dean@example.com</span>
-const emailSpan = document.createElement('span');
-emailSpan.className = "email";
-emailSpan.innerText = "ethel.dean@example.com"
-studentDetailsDiv.appendChild(emailSpan);
-studentListItem.appendChild(studentDetailsDiv);
-//</div>
-//<div class="joined-details">
-
-const joinedDetailsDiv = document.createElement('div');
-joinedDetailsDiv.className = "joined-details";
-//  <span class="date">Joined 12-15-2005</span>
-const dateSpan = document.createElement('span');
-dateSpan.className = "date";
-dateSpan.innerText = "Joined 12-15-2005";
-joinedDetailsDiv.appendChild(dateSpan);
-studentListItem.appendChild(joinedDetailsDiv);
-//</div>
-//</li>
-//TODO: append the list item to the student list ul // <ul class="student-list"></ul>
+   const joinedDetailsDiv = document.createElement('div');
+   joinedDetailsDiv.className = "joined-details";
+   //  <span class="date">Joined 12-15-2005</span>
+   const dateSpan = document.createElement('span');
+   dateSpan.className = "date";
+   dateSpan.innerText = studentObject.registered.date;
+   joinedDetailsDiv.appendChild(dateSpan);
+   studentListItem.appendChild(joinedDetailsDiv);
+   //</div>
+   //</li>
+   return studentListItem;
+}
+//TODO: Call createStudentListItem function and pass in parameters from array
 const studentListUl = document.querySelector('.student-list');
+const studentListItem = createStudentListItem(data[0]);
 studentListUl.appendChild(studentListItem);
 /*
 Create the `addPagination` function
