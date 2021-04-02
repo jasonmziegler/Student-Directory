740fa8d97d14267cd2803bbfbca7f0991aa7f4c3
Jason Ziegler <jason@insightfulautomation.com>
2021-04-02 14:48:41 -0500

    add and clean documentation

--- js/script.js
+++ js/script.js
@@ -16,8 +16,37 @@ This function will create and insert/append the elements needed to display a "pa
 //I think I will need to use slice to display only one section of the array and to not change the array 
 // slice(start, end)
 
-console.log(data);
+//console.log(data);
+
+/**
+ * @type {number}
+ */
+ let numPages = Math.ceil(data.length / 9);
+
+/**
+ * @type {object} 
+ */
+ const pagesList = document.querySelector('.link-list');
+
+/**
+ * This the showPage function
+ * @param {array} list - array of students.
+ * @param {number} pageNumber - page number of the students to display
+ *
+ * @example 
+ * 
+ *    showpage(myArray, 1);
+ */
 function showPage(list, pageNumber) {
+   /**
+   * This the createStudentListItem function (a helper function of showPage)
+   * @param {object} studentObject - an object that contains student information.
+   * @return {object} A document object which contains a div that will display the student details
+   * 
+   * @example 
+   * 
+   *    createStudentListItem(myStudentObject);
+   */
    function createStudentListItem(studentObject) {
       //<li class="student-item cf">
       const studentListItem = document.createElement('li');
@@ -60,61 +89,95 @@ function showPage(list, pageNumber) {
    const studentListUl = document.querySelector('.student-list');
 
    //TODO: Need to make sure that a page number greater than the number of pages doesn't throw an error (may not be necessary)
+   /**
+    * @type {number}
+    */
    let start = (pageNumber * 9) - 9;
+   /**
+    * @type {number}
+    */
    let end = pageNumber * 9;
+   /**
+    * @type {array}
+    */
    let studentsPage = list.slice(start, end);
    for (let i = 0; i < studentsPage.length; i++) {
+      /**
+       * @type {object}
+       */
       const studentListItem = createStudentListItem(studentsPage[i]);
       studentListUl.appendChild(studentListItem);
    }
-}
+} // End of showPage function
 
 /*
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
-//TODO: Need to determine how many pages there will be (if there are 17 students then there needs to be 2 pages)
-let numPages = Math.ceil(data.length / 9);
-
+//determine how many pages there will be (if there are 17 students then there needs to be 2 pages)
+/**
+ * This is the addPagination function. This function takes in the number of pages and the current page
+ * and adds the pagination buttons to the bottom of the student page ul
+ *
+ * @param {number} numPages - A string param
+ * @param {number} currentPage A good string
+ *
+ * @example
+ *
+ *     addPagination(5, 1)
+ */
 function addPagination(numPages, currentPage) {
-//TODO: Need to create elements for each page with the correct page number
+//Create elements for each page with the correct page number active
 /*
 <ul class="link-list"> */
+   /**
+    * @type {object}
+    */
    const pagesList = document.querySelector('.link-list');
    
-   /*<!-- Dynamically insert pagination buttons here
-   EXAMPLE - Two pagination buttons, one with active class, one without: --> */
-   //<li>
+   /**
+    * This is the createPageButton function. ( a helper function for addPagination ) This function creates a single button
+    * for the pagination buttons.
+    *
+    * @param {string} n - A string param
+    * @return {string} A good string
+    *
+    * @example
+    *
+    *     foo('hello')
+    */
    function createPageButton(pageNumber, isActive) {
-   const pageLi = document.createElement('li');
-   //  <button type="button" class="active">1</button>
-   const pageButton = document.createElement('button');
-   pageButton.type = "button";
-   if (isActive) pageButton.className = "active";
-   pageButton.textContent = String(pageNumber);
-   pageLi.appendChild(pageButton);
-   pagesList.appendChild(pageLi);
+      /*<!-- Dynamically insert pagination buttons here
+      EXAMPLE - Two pagination buttons, one with active class, one without: --> */
+      //<li>
+      /**
+       * @type {object}
+       */
+      const pageLi = document.createElement('li');
+      //  <button type="button" class="active">1</button>
+      /**
+       * @type {object}
+       */
+      const pageButton = document.createElement('button');
+      pageButton.type = "button";
+      if (isActive) pageButton.className = "active";
+      pageButton.textContent = String(pageNumber);
+      pageLi.appendChild(pageButton);
+      pagesList.appendChild(pageLi);
   }
 
   for (let i = 1; i <= numPages; i++) {
      createPageButton(i, (i === currentPage));
   }
-}  
-  //</li>
-  //<li>
-  //  <button type="button">2</button>
-  //</li>
-/*</ul>*/
+}  //end of addPagination Function
 
 
-//TODO: need to set up event listeners that will call the show page function when clicked
-//TODO: make sure that the active class is on the page being displayed and is removed from others when another page is clicked
-
-const pagesList = document.querySelector('.link-list');
-// Call functions
+// Show the first page of students when page loads
 showPage(data, 1);
-// createPageButton(1, true);
+// add the pagination below the student ul
 addPagination(numPages, 1);
+
+// add event listeners to the pagination buttons
 pagesList.addEventListener('click', (e) => {
    //console.log(e.target);
    if (e.target.type === 'button') {
@@ -124,6 +187,9 @@ pagesList.addEventListener('click', (e) => {
       pagesList.innerHTML = '';
       //console.log(e.target);
       // parseInt(e.target.innerText);
+      /**
+       * @type {number}
+       */
       let pageClicked = parseInt(e.target.innerText)
       showPage(data, pageClicked);
       addPagination(numPages, pageClicked);
